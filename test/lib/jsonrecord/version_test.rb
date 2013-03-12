060b950724092f528251f1f46a3eee2c4c8a7aba
Pankaj Doharey <pankajdoharey@gmail.com>
2013-03-12 17:44:31 +0530

    Added Just a simple version test in miniruby, will add more test eventually

---
+++ test/lib/jsonrecord/version_test.rb
@@ -0,0 +1,8 @@
+require_relative '../../test_helper'
+
+describe JSONRecord do
+ 
+  it "should be defined" do
+    JSONRecord::VERSION.wont_be_nil
+  end
+end
---
+++ test/test_helper.rb
@@ -0,0 +1,6 @@
+require 'minitest/autorun'
+require 'minitest/spec'
+require_relative '../lib/JSONRecord'
+
+
+
